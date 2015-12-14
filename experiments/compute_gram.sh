cabb80a3e66fa0850eaaf3132b18b36318423b33
Massimo Carlo Maria <cmassimo@labs.math.unipd.it>
2015-12-14 12:20:09 +0100

    Updates scripts to reflect new normalization features

--- experiments/compute_gram.sh
+++ experiments/compute_gram.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #for dataset in "CAS" "NCI1" "AIDS" "CPDB"; do
-for dataset in "NCI1"; do
+for dataset in "CAS"; do
 for radius in "5" "4" "3" "2"; do
 for lambda in "0.1" "0.5" "0.8" "0.9" "1.0" "1.1" "1.2" "1.3" "1.4" "1.5" "1.8"; do
 
@@ -15,8 +15,8 @@ echo "#!/bin/sh
 
 ### Optionally specifiy destinations for your myprogram output
 ### Specify localhost and an NFS filesystem to prevent file copy errors.
-#PBS -e localhost:${HOME}/prova.err
-#PBS -o localhost:${HOME}/tesi/logs/${dataset}.$1.MATRIX.r$radius.l$lambda.out
+#PBS -e localhost:${HOME}/prova2.err
+###PBS -o localhost:${HOME}/tesi/logs/${dataset}.$1.MATRIX.r$radius.l$lambda.out
 
 ### Set the queue to batch, the only available queue. 
 #PBS -q cluster_short
@@ -27,7 +27,7 @@ echo "#!/bin/sh
 #PBS -l nodes=1:ppn=1:infiniband
 
 ### You should tell PBS how much memory you expect your job will use.  mem=1g or mem=1024
-#PBS -l mem=16g
+#PBS -l mem=2g
 
 ### You can override the default 1 hour real-world time limit.  -l walltime=HH:MM:SS
 ### Jobs on the public clusters are currently limited to 10 days walltime.
@@ -39,9 +39,9 @@ echo "#!/bin/sh
 
 cd $HOME/cluster_bundle/scikit-learn-graph/
 
-python -m scripts/calculate_matrix_allkernels ${dataset} $radius $lambda grams/$dataset/k$1.r$radius.l$lambda.mtx $1"> $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.gram.job
+python -m scripts/calculate_matrix_allkernels ${dataset} $radius $lambda grams/$dataset/k$1.r$radius.l$lambda.tanh$3.mtx $1 $2 $3"> $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.tanh$3.gram.job
 
-qsub $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.gram.job
+qsub $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.tanh$3.gram.job
 
 done
 done
--- experiments/compute_nest.sh
+++ experiments/compute_nest.sh
@@ -3,8 +3,9 @@
 #for dataset in "CAS" "NCI1" "AIDS" "CPDB"; do
 for dataset in "CAS"; do
 #for radius in "5" "4" "3" "2"; do
-for radius in "5"; do
-for lambda in "0.1" "0.5" "0.8" "0.9" "1.0" "1.1" "1.2" "1.3" "1.4" "1.5" "1.8"; do
+for radius in "3"; do
+#for lambda in "0.1" "0.5" "0.8" "0.9" "1.0" "1.1" "1.2" "1.3" "1.4" "1.5" "1.8"; do
+for lambda in "0.1" "0.5" "0.8"; do
 for C in "0.0001" "0.001" "0.01" "0.1" "1.0" "10.0" "100.0" "1000.0"; do
 
 echo "#!/bin/sh
@@ -16,7 +17,7 @@ echo "#!/bin/sh
 
 ### Optionally specifiy destinations for your myprogram output
 ### Specify localhost and an NFS filesystem to prevent file copy errors.
-#PBS -e localhost:${HOME}/prova.err
+#PBS -e localhost:${HOME}/tesi/logs/err/${dataset}.$1.MATRIX.r$radius.l$lambda.nested.err
 #PBS -o localhost:${HOME}/tesi/logs/${dataset}.$1.MATRIX.r$radius.l$lambda.nested.out
 
 ### Set the queue to batch, the only available queue. 
@@ -28,7 +29,7 @@ echo "#!/bin/sh
 #PBS -l nodes=1:ppn=1:infiniband
 
 ### You should tell PBS how much memory you expect your job will use.  mem=1g or mem=1024
-#PBS -l mem=8g
+#PBS -l mem=12g
 
 ### You can override the default 1 hour real-world time limit.  -l walltime=HH:MM:SS
 ### Jobs on the public clusters are currently limited to 10 days walltime.
@@ -40,7 +41,7 @@ echo "#!/bin/sh
 
 cd $HOME/cluster_bundle/scikit-learn-graph/
 
-python ../cross_validation_from_matrix.py grams/${dataset}/k$1.r$radius.l$lambda.mtx.svmlight $C cvres/${dataset}/k$1.r$radius.l$lambda"> $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.$C.nested.job
+python $HOME/cluster_bundle/master-thesis/experiments/cross_validation_from_matrix.py grams/${dataset}/k$1.r$radius.l$lambda.mtx.svmlight $C cvres/${dataset}/k$1.r$radius.l$lambda"> $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.$C.nested.job
 
 qsub $HOME/tesi/jobs/${dataset}.$radius.$lambda.$1.$C.nested.job
 
