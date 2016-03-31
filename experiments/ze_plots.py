8643ca2c50b8d32ac17d55832b8e1e48d61429a8
Carlo Maria Massimo <cmassim@gmail.com>
2016-04-01 00:30:33 +0200

    Chapter 4 and revisions

--- experiments/ze_plots.py
+++ experiments/ze_plots.py
@@ -56,21 +56,22 @@ x = np.array([12.086722, 15.97398, 2.134117, 299.721692, 28.09628])
 #x1 = np.array([32.597597, 58.937005, 26.602846, 4.154025, 660.222288])
 x1 = np.array([26.602846, 32.597597, 4.154025, 660.222288, 58.937005])
 ratios = x/x1
-x1 = map(lambda y: math.log(y), x1)
-x = [x1[i]*ratios[i] for i in range(len(x1))]
+#x1 = map(lambda y: log(y), x1)
+#x = [x1[i]*ratios[i] for i in range(len(x1))]
 bins = np.array(range(5))
 #ks = ['CAS', 'NCI1', 'AIDS', 'CPDB', 'GDD']
 ks = ['AIDS', 'CAS', 'CPDB', 'GDD', 'NCI1']
-
-plt.clf()
+fig = plt.figure()
+ax = fig.add_subplot(1,1,1)
 width = 0.2
 p0 = plt.bar(bins-0.1, x, width, color='m', align='center', tick_label=ks)
 p1 = plt.bar(bins, x1, width, color='c', align='edge', tick_label=ks)
-plt.ylabel('time in seconds (log)')
-plt.xlabel('dataset')
-plt.title('Kernel computation performances')
-plt.legend((p0[0], p1[0]), ('incremental', 'sequential'), loc='upper left')
-#plt.show()
+ax.set_ylabel('time in seconds')
+ax.set_xlabel('dataset')
+ax.set_yscale('log')
+ax.set_title('Kernel computation performances')
+ax.legend((p0[0], p1[0]), ('incremental', 'sequential'), loc='upper left')
+plt.show()
 
 plt.savefig('kernel_times.pdf')
 
@@ -105,62 +106,6 @@ plt.title('Methods total computation times analysis')
 plt.legend((p0[0], p1[0], p2[0]), ('MKL ($TCK_{ST}$ {\small orthogonalized})', 'MKL ($TCK_{ST}$)', 'SVM ($TCK_{ST}$)'), loc='upper left')
 plt.show()
 
-# media e std tempi tra parametri
-#x = [range(1+i,13+i+1,3) for i in range(3)]
-#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-#width=0.2
-#x = np.array(range(1,6))
-#y = [[] for i in range(3)]
-#stds = [[] for i in range(3)]
-##ks = ['', '', '1503 (AIDS)', '', '', '4337 (CAS)', '', '', '684 (CPDB)', '', '','1178 (GDD)', '', '', '4110 (NCI1)', '', '']
-##ks = ['', '', '4337 (CAS)', '', '', '4110 (NCI1)', '', '', '1503 (AIDS)', '', '','1178 (GDD)', '', '', '684 (CPDB)', '', '']
-#ks = ['4337\n(CAS)', '4110\n(NCI1)', '1503\n(AIDS)', '1178\n(GDD)', '684\n(CPDB)']
-#ks = [k for k in reversed(ks)]
-##indices=[6, 3, 4, 1, 5, 2, 1, 0]
-#indices2=[2, 3, 0, 4, 1] 
-#y[0] = np.array(map(lambda x: log(x), [1206.69, 25545.21, 160.81, 474.45, 21271.12])) 
-#stds[0] = np.array(map(lambda x: log(x), [148.27, 3550.58, 17.02, 56.17, 3095.15]))
-#y[1] = np.array(map(lambda x: log(x), [1384.66, 27240.70, 191.20, 536.91, 22898.62]))
-#stds[1] = np.array(map(lambda x: log(x), [172.73, 3345.26, 18.01, 59.30, 3778.88]))
-#y[2] = np.array(map(lambda x: log(x), [3731.97, 36011.18, 976.86, 412.32, 33020.56]))
-#stds[2] = np.array(map(lambda x: log(x), [4567.92, 42964.59, 1172.59, 115.27, 37706.40]))
-#plt.clf()
-#p0 = plt.bar(x, y[0][indices2], width, color='c')
-#p1 = plt.bar(x+width, y[1][indices2], width, color='m')
-#p2 = plt.bar(x+2*width, y[2][indices2], width, color='y')
-#plt.errorbar(x+width/2,y[0][indices2],yerr=stds[0][indices2], linestyle="None")
-#plt.errorbar(x+width*(3/2.),y[1][indices2],yerr=stds[1][indices2], linestyle="None")
-#plt.errorbar(x+width*(5/2.),y[2][indices2],yerr=stds[2][indices2], linestyle="None")
-#plt.xticks(np.array(range(1,6))+(3/2.)*width, ks)#, size='small')
-##plt.xticks(range(17), ks)#, size='small')
-#plt.ylabel('mean time and std in seconds (log)')
-#plt.xlabel('number of records (dataset)')
-#plt.title('Methods computation times analysis\n\large for the three methods using $TCK_{ST}$')
-#plt.legend((p0[0], p1[0], p2[0]), ('MKL ($TCK_{ST}$ {\small orthogonalized})', 'MKL ($TCK_{ST}$)', 'SVM ($TCK_{ST}$)'), loc='upper left')
-#plt.show()
-
-# plot tempi totali.
-#rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
-#width=0.2
-#x = np.array(range(1,6))
-#y = [[] for i in range(3)]
-#ks = ['1503\n(AIDS)', '4337\n(CAS)', '684\n(CPDB)', '1178\n(GDD)', '4110\n(NCI1)']
-#indices=[2, 3, 0, 4, 1]
-#indices2=[2, 3, 0, 4, 1] 
-#y[0] = np.array(map(lambda x: log(x), [13273.57, 280997.32, 1768.88, 5219, 233982.28]))
-#y[1] = np.array(map(lambda x: log(x), [15231.23, 299647.71, 2103.2, 5906.0099, 251884.87]))
-#y[2] = np.array(map(lambda x: log(x), [29855.75, 288089.46, 7814.8899, 3298.54, 264164.47]))
-#plt.clf()
-#p0 = plt.bar(x, y[0][indices2], width, color='c')
-#p1 = plt.bar(x+width, y[1][indices2], width, color='m')
-#p2 = plt.bar(x+2*width, y[2][indices2], width, color='y')
-#plt.xticks(np.array(range(1,6))+(3/2.)*width, np.array(ks)[indices])#, size='small')
-#plt.ylabel('total time in seconds (log)')
-#plt.xlabel('number of records (dataset)')
-#plt.title('\Large Nested 10-fold cross-validation times\n \large for the three methods using $TCK_{ST}$')
-#plt.legend((p0[0], p1[0], p2[0]), ('MKL ($TCK_{ST}$ {\small orthogonalized})', 'MKL ($TCK_{ST}$)', 'SVM ($TCK_{ST}$)'), loc='upper left')
-#plt.show()
-
 # plot tempi totali.
 rc('font',**{'family':'sans-serif','sans-serif':['Helvetica']})
 width=0.25
@@ -173,28 +118,46 @@ indices2=[4, 1]
 #y[0] = np.array(map(lambda x: x, [13273.57, 280997.32, 1768.88, 5219, 233982.28]))
 #y[1] = np.array(map(lambda x: x, [15231.23, 299647.71, 2103.2, 5906.0099, 251884.87]))
 #y[2] = np.array(map(lambda x: x, [29855.75, 288089.46, 7814.8899, 3298.54, 264164.47]))
-y[0] = np.array(map(lambda x: x, [10235.5,217526.46, 1347.84, 4027.45,182243.92]))
-y[1] = np.array(map(lambda x: x, [11799.46,229147.23,1601.27,4552.36,198607.11]))
-y[2] = np.array(map(lambda x: x, [29855.75,288089.45999,7814.8899,3298.54,264164.4699]))
+# WL TIMES
+y[0] = np.array([8402.91,0.0,911.18,3036.13,160413.38])
+y[1] = np.array([8730.86,0.0,1030.77,3008.87,172659.05])
+y[2] = np.array([2757.74,0.0,710.85,1195.20,0.0])
 plt.clf()
 plt0 = plt.subplot(1, 2, 1)
-plt.suptitle('\Large Nested 10-fold cross-validation times\n \large for the three methods using $TCK_{ST}$')
-p0 = plt0.bar(x1, y[0][indices], width, color='c')
+#plt.suptitle('\Large Nested 10-fold cross-validation times\n \large for the three methods using $TCK_{ST}$')
+#plt.suptitle('\Large Nested 10-fold cross-validation times\n \large for the three methods using $TCK_{ST+}$')
+plt.suptitle('\Large Nested 10-fold cross-validation times\n \large for the three methods using the $WLC$ kernel')
+p0 = plt0.bar(x1+2*width, y[0][indices], width, color='c')
 p1 = plt0.bar(x1+width, y[1][indices], width, color='m')
-p2 = plt0.bar(x1+2*width, y[2][indices], width, color='y')
+p2 = plt0.bar(x1, y[2][indices], width, color='y')
 plt.xticks(np.array(range(1,4))+(3/2.)*width, np.array(ks)[indices])#, size='small')
 plt.ylabel('total time in seconds')
-plt.xlabel('number of records (dataset)')
+plt.xlabel('number of samples (dataset)')
 width=0.15
 plt1 = plt.subplot(1, 2, 2)
-p0 = plt1.bar(x2, y[0][indices2], width, color='c')
+p0 = plt1.bar(x2+2*width, y[0][indices2], width, color='c')
 p1 = plt1.bar(x2+width, y[1][indices2], width, color='m')
-p2 = plt1.bar(x2+2*width, y[2][indices2], width, color='y')
+p2 = plt1.bar(x2, y[2][indices2], width, color='y')
 plt.xticks(np.array(range(1,3))+(3/2.)*width, np.array(ks)[indices2])#, size='small')
-#plt.ylabel('total time in seconds')
-plt.xlabel('number of records (dataset)')
-#plt.figlegend((p0[0], p1[0], p2[0]),
-#        ('MKL ($TCK_{ST}$ {\small orthogonalized})', 'MKL ($TCK_{ST}$)', 'SVM ($TCK_{ST}$)'),
-#        loc='upper left', ncol=3, mode="expand", borderaxespad=1.)
-plt0.legend((p0[0], p1[0], p2[0]), ('\small MKL ($TCK_{ST}$ {\scriptsize orthogonal})', '\small MKL ($TCK_{ST}$)', '\small SVM ($TCK_{ST}$)'), loc='upper left', borderaxespad=.5)
+plt.xlabel('number of samples (dataset)')
+#plt0.legend((p2[0], p1[0], p0[0]), ('$(TCK_{ST})^{hs}$', '$(TCK_{ST})^c$', '$(TCK_{ST})^{oc}$'), loc='upper left', borderaxespad=.5)
+#plt0.legend((p2[0], p1[0], p0[0]), ('$(TCK_{ST+})^{hs}$', '$(TCK_{ST+})^c$', '$(TCK_{ST+})^{oc}$'), loc='upper left', borderaxespad=.5)
+plt0.legend((p2[0], p1[0], p0[0]), ('$(WLC)^{hs}$', '$(WLC)^c$', '$(WLC)^{oc}$'), loc='upper left', borderaxespad=.5)
+plt0.grid(True, axis='y')
+plt1.grid(True, axis='y')
 plt.show()
+
+#ST TIMES
+y[0] = np.array([1347.84,10235.5,4027.45,182243.92,217526.46])
+y[1] = np.array([1601.27,411799.46,552.36,198607.11,229147.23])
+y[2] = np.array([7814.8899,329855.75,298.54,264164.4699,288089.45999])
+
+#STP TIMES
+y[0] = np.array([10383.54,0.0,1435.74,4463.23,0.0])
+y[1] = np.array([11528.67,233809.02,1884.27,4641.18,194816.36])
+y[2] = np.array([21109.49,226882.42,5477.92,3503.45,0.0])
+
+# WL TIMES
+y[0] = np.array([8402.91,0.0,911.18,3036.13,160413.38])
+y[1] = np.array([8730.86,0.0,1030.77,3008.87,172659.05])
+y[2] = np.array([2757.74,0.0,710.85,1195.20,0.0])
---
+++ experiments/ze_plots_odd.py
@@ -0,0 +1,82 @@
+import sys
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import rc
+from matplotlib.colors import colorConverter
+from matplotlib.ticker import LinearLocator, StrMethodFormatter
+import re
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib.collections import PolyCollection
+from matplotlib.colors import colorConverter
+from matplotlib.colors import LightSource
+from matplotlib import cm
+rc('font', **{'family':'serif','serif':['Palatino']})
+rc('text', usetex=True)
+#sys.argv.append("../../cluster_backup/dists/CAS_mklst")
+#sys.argv.append("ODDST")
+
+plt.clf()
+
+fig = plt.figure(figsize=(5, 5))
+ax = fig.add_subplot(111, projection='3d')
+
+# MKL matrices weights distribution
+weights_raw = np.loadtxt(sys.argv[1], delimiter=',', dtype='str').T
+kernel = sys.argv[2]
+method = sys.argv[3]
+dataset = sys.argv[4]
+maxheight = int(sys.argv[5])
+wdict = {re.search('[A-Z]*.r\d+.*', w[0]).group(0):np.array(w[1:], dtype='float64') for w in weights_raw}
+Lambdas = [l/10. for l in range(8)]
+nmat = len(wdict)
+h=range(1,maxheight)
+
+xs = np.array([np.array(range(nmat), dtype='float_') for i in range(len(Lambdas))])
+ys = np.array([np.zeros(nmat)+(i/10.) for i in range(len(Lambdas))])
+
+plots = []
+labels = []
+tops = []
+ticks = ['' for i in range(nmat)]
+topjs = []
+
+for i, L in enumerate(Lambdas):
+    weights = []
+    for r in h:
+        for j, l in enumerate(range(r+1)):
+            ck = kernel+".r"+str(r)+".depth"+str(l)
+            cw = wdict[ck][i]
+            weights.append(cw)
+    labels.append("$\Lambda$ "+str(L))
+    plots.append(np.array(weights, dtype='float64'))
+#    print sum(weights)
+
+x = xs
+y = ys
+z = plots
+#print x.shape, y.shape, len(z)
+#ax.set_yticks(range(nmat),map(lambda x: str(x), np.array(range(12))/10.))
+fm = StrMethodFormatter('%d')
+ll=LinearLocator(len(Lambdas))
+ll.tick_values(0.0,1.0)
+ax.w_yaxis.set_major_locator(ll) 
+ax.view_init(20,-95)
+
+cm = plt.get_cmap('gist_rainbow')
+colors = [cm(1.*i/len(Lambdas), alpha=0.5) for i in range(len(Lambdas))]
+for i in range(nmat):
+    colors.append((0,0,0,.35))
+#cm = plt.get_cmap('gist_earth')
+surf = ax.plot_wireframe(x, y, z, rstride=1, cstride=1, colors=colors, linewidth=1, antialiased=True)
+#surf = ax.plot_surface(x, y, z, rstride=1, cstride=1, linewidth=1, antialiased=True, cmap=cm.coolwarm)
+
+ax.set_xlabel('matrix index')
+ax.set_xlim3d(0, nmat)
+ax.set_ylabel("$\Lambda$")
+ax.set_ylim3d(0, 0.7)
+ax.set_zlabel('kernel weight')
+ax.set_zlim3d(0, 0.30)
+plt.title("Kernel weights distribution\n for $("+kernel+")^{"+method+"}$ on the "+dataset+" dataset")
+plt.tight_layout()
+plt.show()
+#plt.savefig("kca_"+dataset+"_"+kernel+"_"+method+".pdf")
